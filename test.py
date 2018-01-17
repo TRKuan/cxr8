49e75c09072288a70fd865be8dcda99d40723be7
TR <tianruei.kuan@gmail.com>
2018-01-18 00:21:13 +0800

    update test.py for resnet

--- test.py
+++ test.py
@@ -8,11 +8,10 @@ import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
 from torch.utils.data import Dataset, DataLoader
-#import cv2
 import torch.utils.model_zoo as model_zoo
 import time
 import os
-
+import random
 from PIL import Image
 
 
@@ -36,7 +35,6 @@ class CXRDataset(Dataset):
 
     def __getitem__(self, idx):
         img_name = os.path.join(self.root_dir, self.labels_csv.ix[idx, 0])
-        #image = cv2.imread(img_name)
         image = Image.open(img_name).convert('RGB')
 
         if self.transform:
@@ -48,18 +46,18 @@ class CXRDataset(Dataset):
         return sample
 
 def loadData(batch_size):
-    trans = torchvision.transforms.Compose([transforms.Resize(224), torchvision.transforms.ToTensor()])
-    image_datasets = {x: CXRDataset(label_path[x], data_dir, transform = trans)for x in ['train', 'val']}
+    trans = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+    image_datasets = {x: CXRDataset(label_path[x], data_dir, transform = trans)for x in ['val']}
     dataloders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=False, num_workers=4)
-                  for x in ['train', 'val']}
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-    print('Training data: {}\nValidation data: {}'.format(dataset_sizes['train'], dataset_sizes['val']))
+                  for x in ['val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['val']}
+    print('Validation data: {}'.format(dataset_sizes['val']))
 
-    class_names = image_datasets['train'].classes
+    class_names = image_datasets['val'].classes
     return dataloders, dataset_sizes, class_names
 
 def test_model(model):
-    batch_size = 500 
+    batch_size = 24
     since = time.time()
     dataloders, dataset_sizes, class_names = loadData(batch_size)
 
@@ -70,17 +68,17 @@ def test_model(model):
     labelList = []
     num=0
     # Iterate over data.
-    for data in dataloders['val']:
+    for idx, data in enumerate(dataloders['val']):
         # get the inputs
         inputs = data['image']
         labels = data['label']
 
         #wrap them in Variable
         if use_gpu:
-            inputs = Variable(inputs.cuda())
-            labels = Variable(labels.cuda())
+            inputs = Variable(inputs.cuda(), volatile=True)
+            labels = Variable(labels.cuda(), volatile=True)
         else:
-            inputs, labels = Variable(inputs), Variable(labels)
+            inputs, labels = Variable(inputs, volatile=True), Variable(labels, volatile=True)
                 
         outputs = model(inputs)
         out_data = outputs.data
@@ -90,15 +88,14 @@ def test_model(model):
         for i in range(out_data.shape[0]):
             outputList.append(out_data[i].tolist())
             labelList.append(labels[i].tolist())
-        num = num + 1
-        if(num%20 == 0):
-            print('{:.2f}%'.format(100*num/len(dataloders['val'])))
+        
+        if idx%20 == 0 and idx!=0:
+            print('{:.2f}%'.format(100*idx/len(dataloders['val'])), end='\r')
 
-    print()
-    #labelList.append([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    #outputList.append([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    print('Sample outputs-----------------')
+    base = random.randint(0, dataset_sizes['val']-min(30, len(outputList)))
     for i in range(min(30, len(outputList))):
-        print('{} output: {}\nlabel: {}\n---------------'.format(i, ['{:.2f}'.format(item) for item in outputList[i]], labelList[i]))
+        print('{} output: {}\nlabel: {}\n---------------'.format(i+base, ['{:.4f}'.format(item) for item in outputList[i+base]], labelList[i+base]))
                 
     epoch_auc_ave = roc_auc_score(np.array(labelList), np.array(outputList))
     epoch_auc = roc_auc_score(np.array(labelList), np.array(outputList), average=None)
@@ -109,73 +106,48 @@ def test_model(model):
         print('{}: {:.4f} '.format(c, epoch_auc[i]))
     print()
 
-
-class Vgg(nn.Module):
+class Model(nn.Module):
     def __init__(self):
-        super(Vgg, self).__init__()
-        self.model_ft = models.vgg16(pretrained=True)
-        self.model_ft = nn.Sequential(*list(self.model_ft.features.children())[:-1])
+        super(Model, self).__init__()
+        self.model_ft = models.resnet50(pretrained=True)
         for param in self.model_ft.parameters():
             param.requires_grad = False
 
         self.transition = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, padding=1, stride=1, bias=False),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(2048, 2048, kernel_size=3, padding=1, stride=1, bias=False),
         )
         self.globalPool = nn.Sequential(
             nn.MaxPool2d(32)
         )
         self.prediction = nn.Sequential(
-            nn.Linear(512, 14),
-            nn.Sigmoid()
-        )
-    
-    def forward(self, x):
-        x = self.model_ft(x)#512x64x64
-        x = self.transition(x)#512x32x32
-        x = self.globalPool(x)#512x1x1
-        x = x.view(x.size(0), -1)#512
-        x = self.prediction(x)#14
-        return x
-
-class Alexnet(nn.Module):
-    def __init__(self):
-        super(Alexnet, self).__init__()
-        self.model_ft = models.alexnet(pretrained=True)
-        self.model_ft = nn.Sequential(*list(self.model_ft.features.children())[:-1])
-        for param in self.model_ft.parameters():
-            param.requires_grad = False
-
-        self.transition = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3, padding=2, stride=1, bias=False),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-        )
-        self.globalPool = nn.Sequential(
-            nn.MaxPool2d(32)
-        )
-        self.prediction = nn.Sequential(
-            nn.Linear(256, 14),
+            nn.Linear(2048, 14),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        x = self.model_ft(x)#256x64x64
-        x = self.transition(x)#256x32x32
-        x = self.globalPool(x)#256x1x1
-        x = x.view(x.size(0), -1)#256
+        x = self.model_ft.conv1(x)
+        x = self.model_ft.bn1(x)
+        x = self.model_ft.relu(x)
+        x = self.model_ft.maxpool(x)
+
+        x = self.model_ft.layer1(x)
+        x = self.model_ft.layer2(x)
+        x = self.model_ft.layer3(x)
+        x = self.model_ft.layer4(x)
+
+
+        x = self.transition(x)
+        x = self.globalPool(x)
+        x = x.view(x.size(0), -1)
         x = self.prediction(x)#14
         return x
 
 
 if __name__ == '__main__':
-    model = models.alexnet(pretrained=True)
-    model.classifier = nn.Sequential(
-        nn.Linear(256*6*6, 14),
-        nn.Sigmoid()
-    )
+    model = Model()
     if use_gpu:
         model = model.cuda()
-        model = torch.nn.DataParallel(model).cuda()
-    model.load_state_dict(torch.load(os.path.join(save_dir, "alexnet_v2.pth"))) 
+        #model = torch.nn.DataParallel(model).cuda()
+    model.load_state_dict(torch.load(os.path.join(save_dir, "resnet50.pth"))) 
     model = test_model(model.cuda())
 
