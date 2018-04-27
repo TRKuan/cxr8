a41f014cc7b6a726bb7e67a99adef96a75a4c0cc
TR <tianruei.kuan@gmail.com>
2018-04-28 01:55:42 +0800

    update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,4 +1,3 @@
-import torch
 from torch.utils.data import Dataset
 import pandas as pd
 from PIL import Image
@@ -7,23 +6,26 @@ import os
 
 class CXRDataset(Dataset):
 
-    def __init__(self, csv_file, root_dir, transform = None):
-        self.labels_csv = pd.read_csv(csv_file, header=0)
-        self.root_dir = root_dir
+    def __init__(self, index_dir, image_dir, type = 'train', transform = None):
+        self.index_dir = index_dir
+        self.image_dir = image_dir
+        self.label_index = pd.read_csv(index_dir, header=0)
         self.transform = transform
-        self.classes = pd.read_csv(csv_file, header=None,nrows=1).ix[0, :].as_matrix()
-        self.classes = self.classes[1:]
+        self.classes = pd.read_csv(index_dir, header=None,nrows=1).ix[0, :].as_matrix()[1:]
 
     def __len__(self):
-        return len(self.labels_csv)
+        return len(self.label_index)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir, self.labels_csv.ix[idx, 0])
-        image = Image.open(img_name).convert('RGB')
+        img_dir = os.path.join(self.image_dir, self.label_index.ix[idx, 0])
+        image = Image.open(img_dir).convert('RGB')
         if self.transform:
             image = self.transform(image)
-        label = self.labels_csv.ix[idx, 1:].as_matrix().astype('float')
-        label = torch.from_numpy(label).type(torch.FloatTensor)
-        sample = {'image': image, 'label': label}
+        label = self.label_index.ix[idx, 1:].as_matrix().astype('int')
+        return image, label
 
-        return sample
+if __name__ == '__main__':
+    index_dir = 'dataset/label_index.csv'
+    image_dir = 'dataset/images'
+    dataset = CXRDataset(index_dir, image_dir)
+    print(dataset[0])
