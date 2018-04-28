0e963108f439e9a4dce74a99afdb289b62ab0fb9
TR <tianruei.kuan@gmail.com>
2018-04-28 15:41:27 +0800

    working dataset v1

--- dataset.py
+++ dataset.py
@@ -6,26 +6,51 @@ import os
 
 class CXRDataset(Dataset):
 
-    def __init__(self, index_dir, image_dir, type = 'train', transform = None):
-        self.index_dir = index_dir
-        self.image_dir = image_dir
-        self.label_index = pd.read_csv(index_dir, header=0)
+    def __init__(self, root_dir, dataset_type = 'train', transform = None):
+        if dataset_type not in ['train', 'val', 'test']:
+            raise ValueError("No such type, must be 'train', 'val', or 'test'")
+        
+        self.image_dir = os.path.join(root_dir, 'images')
         self.transform = transform
+        index_dir = os.path.join(root_dir, 'label_index.csv')
         self.classes = pd.read_csv(index_dir, header=None,nrows=1).ix[0, :].as_matrix()[1:]
-
+        self.label_list = []
+        
+        # read in the image names
+        image_name_list_dir = os.path.join(root_dir, dataset_type+'_list.txt')
+        with open(image_name_list_dir) as f:
+            image_name_list = f.read().split('\n')
+        
+        # find the corresponding label
+        label_index = pd.read_csv(index_dir, header=0)
+                
+        # generate the label list
+        pos = 0
+        for i in range(len(image_name_list)):
+            while image_name_list[i] != label_index.ix[pos, 0]:
+                pos += 1
+                if pos >= len(label_index):
+                    raise ValueError("The image does not have a corresponding label")
+            label = (image_name_list[i], label_index.ix[pos, 1:].as_matrix().astype('int'))
+            self.label_list.append(label)
+            if i%1000 == 0:
+                print('\rDataset loading {:.2f}%'.format(100*i/len(image_name_list)), end='\r')
+        
     def __len__(self):
-        return len(self.label_index)
+        return len(self.label_list)
 
     def __getitem__(self, idx):
-        img_dir = os.path.join(self.image_dir, self.label_index.ix[idx, 0])
+        name = self.label_list[idx][0]
+        img_dir = os.path.join(self.image_dir, name)
         image = Image.open(img_dir).convert('RGB')
         if self.transform:
             image = self.transform(image)
-        label = self.label_index.ix[idx, 1:].as_matrix().astype('int')
-        return image, label
-
-if __name__ == '__main__':
-    index_dir = 'dataset/label_index.csv'
-    image_dir = 'dataset/images'
-    dataset = CXRDataset(index_dir, image_dir)
-    print(dataset[0])
+        
+        label = self.label_list[idx][1]
+        return image, label, name
+    
+dataset = CXRDataset('dataset', dataset_type='train')
+print(dataset[110])
+print(dataset[12])
+print(dataset[23])
+print(dataset[34])
