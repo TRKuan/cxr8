9bbab73c772888bd9293e84c81d78035a1048c71
TR <tianruei.kuan@gmail.com>
2018-04-28 14:45:43 +0800

    update label_gen list sorted and patient split guaranty

--- label_gen.py
+++ label_gen.py
@@ -73,16 +73,48 @@ if __name__ == '__main__':
     print("Label index generated at '%s'"%(index_dist_dir))
     
     # Split training list and  validation list
+    train_list = []
+    val_list = []
     with open(train_val_list_dir) as f:
         with open(train_list_dir, 'w+') as wf_t:
             with open(val_list_dir, 'w+') as wf_v:
                 image_name_list = f.read().split('\n')
-                random.shuffle(image_name_list)
+                
+                # group the same patients together to guaranty no patient overlap between splits
+                patients = []
+                last = ''
                 for i in range(len(image_name_list)):
-                    if i < len(image_name_list)*(7/8):
-                        wf_t.write(image_name_list[i]+'\n')
+                    if last == image_name_list[i][:8]:
+                        patients[-1].append(image_name_list[i])
                     else:
-                        wf_v.write(image_name_list[i]+'\n')
+                        patients.append([image_name_list[i]])
+                    last = image_name_list[i][:8]
+                
+                # shuffle
+                random.shuffle(patients)
+                
+                # split them into train and val
+                train_list = []
+                val_list = []
+                train_num = 0
+                for i in range(len(patients)):
+                    if train_num < len(image_name_list)*(7/8):
+                        train_list += patients[i]
+                        train_num += len(patients[i])
+                    else:
+                        val_list += patients[i]
+                        
+                # sort the list
+                train_list.sort()
+                val_list.sort()
+                
+                # write file
+                for data in train_list[:-1]:
+                    wf_t.write(data+'\n')
+                wf_t.write(train_list[-1])
+                for data in val_list[:-1]:
+                    wf_v.write(data+'\n')
+                wf_v.write(val_list[-1])
 
     print("Training list generated at '%s'"%(train_list_dir))
     print("Validation list generated at '%s'"%(val_list_dir))
