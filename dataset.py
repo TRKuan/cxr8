f51ae4c3ea685492f7b8ef94b21c9c3c0445fdfb
TR <tianruei.kuan@gmail.com>
2018-05-16 20:35:05 +0800

    data argumentation for bbox dataset

--- dataset.py
+++ dataset.py
@@ -1,9 +1,12 @@
 import torch
 from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.transforms import functional as F
 import pandas as pd
 import numpy as np
 from PIL import Image
 import os
+import random
 
 disease_categories = {
         'Atelectasis': 0,
@@ -69,13 +72,14 @@ class CXRDataset(Dataset):
     
 class CXRDataset_BBox_only(Dataset):
 
-    def __init__(self, root_dir, transform = None): 
+    def __init__(self, root_dir, transform = None, data_arg=True): 
         self.image_dir = os.path.join(root_dir, 'images')
         self.transform = transform
         self.index_dir = os.path.join(root_dir, 'test'+'_label.csv')
         self.classes = pd.read_csv(self.index_dir, header=None,nrows=1).ix[0, :].as_matrix()[1:9]
         self.label_index = pd.read_csv(self.index_dir, header=0)
         self.bbox_index = pd.read_csv(os.path.join(root_dir, 'BBox_List_2017.csv'), header=0)
+        self.data_arg = data_arg
         
     def __len__(self):
         return len(self.bbox_index)
@@ -84,8 +88,6 @@ class CXRDataset_BBox_only(Dataset):
         name = self.bbox_index.iloc[idx, 0]
         img_dir = os.path.join(self.image_dir, name)
         image = Image.open(img_dir).convert('L')
-        if self.transform:
-            image = self.transform(image)
         label = self.label_index.loc[self.label_index['FileName']==name].iloc[0, 1:9].as_matrix().astype('int')
             
         
@@ -105,5 +107,26 @@ class CXRDataset_BBox_only(Dataset):
                     int(cols.iloc[i, 2]/2): int(cols.iloc[i, 2]/2+cols.iloc[i, 4]/2) #x:x+w
                 ] = 1
                 bbox_valid[disease_categories[cols.iloc[i, 1]]] = 1
+        
+        #argumentation
+        if self.data_arg:
+            image = F.resize(image, 600, Image.BILINEAR)
+            angle = random.uniform(-20, 20)
+            image = F.rotate(image, angle, resample=False, expand=False, center=None)
+            crop_i = random.randint(0, 600 - 512)
+            crop_j = random.randint(0, 600 - 512)
+            image = F.crop(image, crop_i, crop_j, 512, 512)
+            bbox_list = []
+            for i in range(8):
+                bbox_img = Image.fromarray(bbox[i])
+                bbox_img = F.resize(bbox_img, 600, Image.BILINEAR)
+                bbox_img = F.rotate(bbox_img, angle, resample=False, expand=False, center=None)
+                bbox_img = F.crop(bbox_img, crop_i, crop_j, 512, 512)
+                bbox_img = transforms.ToTensor()(bbox_img)
+                bbox_list.append(bbox_img)
+            bbox = torch.cat(bbox_list)
+        
+        if self.transform:
+            image = self.transform(image)
         
         return image, label, name, bbox, bbox_valid
